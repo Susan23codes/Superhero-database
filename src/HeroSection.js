faabbd7cb920a958df55ebcf2063a2270c0d80e8
Susan <susan23codes@gmail.com>
2022-11-04 19:42:05 -0400

    made background image on page

---
+++ src/HeroSection.js
@@ -0,0 +1,11 @@
+
+
+export default function HeroSection() {
+    return (
+        <>
+        <div className="Hero">
+            <h2 style={{fontSize:'3rem', margin: 0}}>Search By Superhero Name:</h2>
+        </div>
+        </>
+    )
+}
